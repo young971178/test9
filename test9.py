d4d10f6ee51466dcf563da75ea256773b212650c
young971178 <young971178@gmail.com>
2026-07-27 07:42:30 +0900

    test9

---
+++ test9.py
@@ -0,0 +1,68 @@
+import streamlit as st
+import random
+
+st.set_page_config(layout="wide")
+
+if 'user_name' not in st.session_state:
+    st.session_state.user_name = random.choice(["박찬호", "박지성", "이영표", "류현진", "손흥민"])
+
+st.title(f"{st.session_state.user_name} 프로님 AXage에 오신 것을 환영합니다.")
+
+st.header("What's New!")
+col1, col2, col3 = st.columns(3)
+
+with col1:
+    st.subheader("GDX 과제 최신 소식")
+    st.info("아라미드 생산2팀에서 A급율을 높이기 위한 극한 과제에 도전하고 있습니다. 함께 응원해 주세요")
+    st.link_button("응원하러 바로가기", "#")
+    
+    st.info("코오드 생산팀의 KIB 열수축응력 개선과제가 성공적으로 완료 되었습니다.")
+    st.link_button("개선성과 보러가기", "#")
+    
+    st.info("GDX 과제정의 Agent 가 새롭게 개발되었습니다. 과제정의서 작성은 이제 Agent 에게 맡기세요")
+    st.link_button("과제정의 Agent 바로가기", "#")
+
+with col2:
+    st.subheader("GDX 교육 On Air~!")
+    st.success("미니탭 리터러시 오프라인 교육 과정이 새롭게 개설 되었습니다. 이론과 경험을 겸비한 엄유범 프로님의 강의를 만끽하세요")
+    st.link_button("미니탭 리터러시 오프라인 교육 더 알아보기", "#")
+    
+    st.success("기술통계 온라인 과정이 리뉴얼 되었습니다. 기존의 이론중심 교육이 아닌 코딩 위주의 실습형 교육을 체험해 보세요")
+    st.link_button("기술통계 온라인 과정 더 알아보기", "#")
+    
+    st.success("머신러닝-모델링 모듈의 강사진에 지덱수 프로님이 합류하셨습니다. 중합공정의 최적화 문제라면 이젠 지덱수 프로님의 강의가 딱이겠지요?")
+    st.link_button("지덱수 프로님의 머신러닝모델링 강의 교안 보러가기", "#")
+
+with col3:
+    st.subheader("새로운 배지 획득을 축하합니다.")
+    st.warning("지덱수 프로님이 머신러닝-모델링 모듈의 골드크라운 배지를 획득하셨습니다. 멋진 사내강사 활동 기대합니다.")
+    st.link_button("👍 좋아요", "#", key="badge_1")
+    
+    st.warning("기냥 프로님이 파이썬리터러시 그린배지를 획득하셨습니다. 골드크라운을 획득하는 그날까지 쭈욱 응원합니다")
+    st.link_button("👍 좋아요", "#", key="badge_2")
+    
+    st.warning("GDX 아카데미에서 AI Automation 사내강사로서 활동하실 인재를 모집합니다.")
+    st.link_button("사내강사 등록신청 바로가기", "#")
+
+st.markdown("---")
+
+col4, col5, col6, col7, col8 = st.columns(5)
+
+with col4:
+    st.link_button("GDX 아카데미 수강신청 바로가기", "https://njptvnmnnutbylghv4zuu2.streamlit.app/")
+
+with col5:
+    st.write("**AXage 는 인사 및 승진제도와 연계됩니다.**")
+    st.link_button("인사제도 확인 바로가기", "#")
+
+with col6:
+    st.link_button("온라인 교육 수강생 응시 바로가기", "https://fqjhmeynfrjdppf4f38vgb.streamlit.app/")
+
+with col7:
+    st.link_button("오프라인 교육 수강생 과제제출 바로가기", "#")
+
+with col8:
+    st.link_button("AXage 불편신고 및 개선건의", "#")
+
+st.markdown("---")
+st.markdown("<h4 style='text-align: center; color: gray;'>측정할 수 없는 것은 개선할 수 없습니다</h4>", unsafe_allow_html=True)
